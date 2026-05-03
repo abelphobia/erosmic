bd2a26648e04270f2f9c2719896ddca780b63a2e
Azeez Mohammed <contactzaidm@gmail.com>
2026-05-02 22:22:17 -0500

    rev playlist

--- lib/pages/playlists_page.dart
+++ lib/pages/playlists_page.dart
@@ -1,13 +1,33 @@
 import 'package:flutter/material.dart';
 
-class PlaylistsPage extends StatelessWidget {
-  const PlaylistsPage({super.key});
+class PlaylistPage extends StatelessWidget {
+  const PlaylistPage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final items = [
+      "Workout Mix",
+      "Study Playlist",
+      "Chill Vibes",
+      "Road Trip",
+      "Relax Mode",
+      "Party Mix",
+    ];
+
     return Scaffold(
-      backgroundColor: Theme.of(context).colorScheme.surface,
-      appBar: AppBar(title: Text("P L A Y L I S T S")),
+      appBar: AppBar(
+        title: const Text("Playlist"),
+        centerTitle: true,
+      ),
+      body: ListView.builder(
+        itemCount: items.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            leading: const Icon(Icons.queue_music),
+            title: Text(items[index]),
+          );
+        },
+      ),
     );
   }
 }
