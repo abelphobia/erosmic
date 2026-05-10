96f4afebc2b4376ccd0f6d87dffc68d48b265f37
abelphobia <abeljcaridad@gmail.com>
2026-05-10 14:24:38 -0500

    imported my_drawer and removed prev. code

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -5,6 +5,7 @@ import 'package:erosmic/pages/genre_page.dart';
 import 'package:erosmic/models/track_info.dart';
 import 'package:provider/provider.dart';
 import 'package:erosmic/models/song.dart';
+import 'package:erosmic/categories/my_drawer.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -170,56 +171,8 @@ class _HomePageState extends State<HomePage> {
   // ── Drawer ──────────────────────────────────────────────────────────────────
 
   Widget buildAppDrawer(BuildContext context) {
-    return Drawer(
-      child: ListView(
-        padding: EdgeInsets.zero,
-        children: [
-          const DrawerHeader(
-            decoration: BoxDecoration(color: Color.fromARGB(255, 71, 131, 221)),
-            child: Align(
-              alignment: Alignment.bottomLeft,
-              child: Text(
-                "Erosmic Menu",
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ),
-          ),
-          ListTile(
-            leading: const Icon(Icons.home),
-            title: const Text("Homepage"),
-            onTap: () => Navigator.pop(context),
-          ),
-          ListTile(
-            leading: const Icon(Icons.queue_music),
-            title: const Text("Albums"),
-            onTap: () {
-              Navigator.pop(context);
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (_) => const PlaylistPage()),
-              );
-            },
-          ),
-          ListTile(
-            leading: const Icon(Icons.library_music),
-            title: const Text("Genre"),
-            onTap: () {
-              Navigator.pop(context);
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (_) => const GenrePage()),
-              );
-            },
-          ),
-        ],
-      ),
-    );
+    return const MyDrawer();
   }
-
   // Part of the Section builders
 
   Widget buildSectionTitle(String title) {
