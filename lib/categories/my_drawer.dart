61d5ed05d5c3de9c67fa8dcd23489778dec414a3
abelphobia <abeljcaridad@gmail.com>
2026-05-10 17:19:11 -0500

    merge

--- lib/categories/my_drawer.dart
+++ lib/categories/my_drawer.dart
@@ -1,56 +1,85 @@
 import 'package:flutter/material.dart';
 import 'package:erosmic/pages/genre_page.dart';
 import 'package:erosmic/pages/playlists_page.dart';
+import 'package:erosmic/pages/all_tracks_page.dart';
+import 'package:erosmic/pages/settings_page.dart';
 
-Widget buildAppDrawer(BuildContext context) {
-  return Drawer(
-    child: ListView(
-      padding: EdgeInsets.zero,
-      children: [
-        const DrawerHeader(
-          decoration: BoxDecoration(color: Colors.deepPurple),
-          child: Align(
-            alignment: Alignment.bottomLeft,
-            child: Text(
-              "Erosmic Menu",
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
+class MyDrawer extends StatelessWidget {
+  const MyDrawer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: [
+          const DrawerHeader(
+            decoration: BoxDecoration(color: Color.fromARGB(255, 71, 131, 221)),
+            child: Align(
+              alignment: Alignment.bottomLeft,
+              child: Text(
+                "Erosmic Menu",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
             ),
           ),
-        ),
-        ListTile(
-          leading: const Icon(Icons.home),
-          title: const Text("Homepage"),
-          onTap: () {
-            Navigator.pop(context);
-          },
-        ),
-        ListTile(
-          leading: const Icon(Icons.queue_music),
-          title: const Text("Playlist"),
-          onTap: () {
-            Navigator.pop(context);
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => const PlaylistPage()),
-            );
-          },
-        ),
-        ListTile(
-          leading: const Icon(Icons.library_music),
-          title: const Text("Genre"),
-          onTap: () {
-            Navigator.pop(context);
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => const GenrePage()),
-            );
-          },
-        ),
-      ],
-    ),
-  );
+          ListTile(
+            leading: const Icon(Icons.home),
+            title: const Text("Homepage"),
+            onTap: () {
+              Navigator.pop(context);
+            },
+          ),
+          ListTile(
+            leading: const Icon(Icons.library_music),
+            title: const Text("Playlists"),
+            onTap: () {
+              Navigator.pop(context);
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (_) => const PlaylistPage()),
+              );
+            },
+          ),
+          ListTile(
+            leading: const Icon(Icons.crop_square),
+            title: const Text("Genres"),
+            onTap: () {
+              Navigator.pop(context);
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (_) => const GenrePage()),
+              );
+            },
+          ),
+          ListTile(
+            leading: const Icon(Icons.music_note),
+            title: const Text("All Songs"),
+            onTap: () {
+              Navigator.pop(context);
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (_) => const AllTracksPage()),
+              );
+            },
+          ),
+          ListTile(
+            leading: const Icon(Icons.settings),
+            title: const Text("Settings"),
+            onTap: () {
+              Navigator.pop(context);
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (_) => const SettingsPage()),
+              );
+            },
+          ),
+        ],
+      ),
+    );
+  }
 }
