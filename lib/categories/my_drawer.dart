faaa968f8180e6f9b2f3563f0b56e72223fdb789
abelphobia <abeljcaridad@gmail.com>
2026-05-11 10:38:20 -0500

    annotations added

--- lib/categories/my_drawer.dart
+++ lib/categories/my_drawer.dart
@@ -1,3 +1,6 @@
+/// My drawer is used to sort the categories of features on the sandwhich column.
+/// THese include the all tracks, playlist, genres, settings, and upload page.
+
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:erosmic/pages/genre_page.dart';
@@ -6,17 +9,25 @@ import 'package:erosmic/pages/all_tracks_page.dart';
 import 'package:erosmic/pages/settings_page.dart';
 import 'package:erosmic/models/track_info.dart';
 
+// creates a custom widget that can be place on multiple apps
 class MyDrawer extends StatelessWidget {
-  const MyDrawer({super.key});
+  const MyDrawer({super.key}); // super keys accepts an optional key parameter
 
   @override
   Widget build(BuildContext context) {
+    // Navigation to the main menu
     return Drawer(
+      // A Child can accept a single widget 
       child: ListView(
         padding: EdgeInsets.zero,
+        // Children can accept a list of widgets
         children: [
+          // consts can not be modified once itialized
+          // header of the menu page
           const DrawerHeader(
             decoration: BoxDecoration(color: Color.fromARGB(255, 71, 131, 221)),
+            // because there is a children property, we can set multiple childs
+            // onto this list.
             child: Align(
               alignment: Alignment.bottomLeft,
               child: Text(
@@ -29,24 +40,33 @@ class MyDrawer extends StatelessWidget {
               ),
             ),
           ),
+          // First List (Homepage)
           ListTile(
             leading: const Icon(Icons.home),
             title: const Text("Homepage"),
             onTap: () {
+              // Navigators directs the user to another page on the application
               Navigator.pop(context);
+              // Since there is no direct push, this pushes the app back to the main
+              // page, which in this case would be the homepage.
             },
           ),
+          // Second List (Playlists)
           ListTile(
+            // icons create physical icons, like Caleb Williams
             leading: const Icon(Icons.library_music),
             title: const Text("Playlists"),
-            onTap: () {
+            onTap: () { // Ontap allows to trigger the request to send the user 
               Navigator.pop(context);
+              // Navigator gets pushed to MaterialPageRoute which sends the user
+              // to the PlaylistPage that is created.
               Navigator.push(
                 context,
                 MaterialPageRoute(builder: (_) => const PlaylistPage()),
               );
             },
           ),
+          // Third List (Genres) | Sends the user to genre page
           ListTile(
             leading: const Icon(Icons.crop_square),
             title: const Text("Genres"),
@@ -58,6 +78,7 @@ class MyDrawer extends StatelessWidget {
               );
             },
           ),
+          // Fourth List (All Song) | Sends the user to the AllTracksPage
           ListTile(
             leading: const Icon(Icons.music_note),
             title: const Text("All Songs"),
@@ -69,6 +90,7 @@ class MyDrawer extends StatelessWidget {
               );
             },
           ),
+          // Fifth List (Settings) | Sends the user to settings page
           ListTile(
             leading: const Icon(Icons.settings),
             title: const Text("Settings"),
@@ -80,13 +102,17 @@ class MyDrawer extends StatelessWidget {
               );
             },
           ),
+          // Sixth List (Importing songs)
           ListTile(
             leading: const Icon(Icons.file_upload),
             title: const Text("Import Local Songs"),
             onTap: () {
+              // With the depenencies, we can read and write local files
+              // to play with media on the application 
               Navigator.pop(context);
               context.read<TrackInfo>().pickLocalFiles();
             },
+            // The media imported locally will also be placed on recently added.
           ),
         ],
       ),
