2690a998553574bd15c200ba6ac0eb5b5a6dffe3
abelphobia <abeljcaridad@gmail.com>
2026-05-12 10:32:28 -0500

    added theme_provider

--- lib/pages/settings_page.dart
+++ lib/pages/settings_page.dart
@@ -7,34 +7,16 @@ class SettingsPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Theme.of(context).colorScheme.surface,
-      appBar: AppBar(title: const Text("S E T T I N G S")),
-      body: Container(
-        decoration: BoxDecoration(color: Theme.of(context).colorScheme.primary),
-        padding: const EdgeInsets.all(16.0),
-        margin: const EdgeInsets.all(16.0),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            // dark mode toggle
-            const Text(
-              "Dark Mode",
-              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-            ),
+    final themeProvider = context.watch<ThemeProvider>();
 
-            // switch
-            Switch(
-              value: Provider.of<ThemeProvider>(context).isDarkMode,
-              onChanged: (value) {
-                Provider.of<ThemeProvider>(
-                  context,
-                  listen: false,
-                ).toggleTheme();
-              },
-            ),
-          ],
-        ),
+    return Scaffold(
+      appBar: AppBar(title: const Text("Settings")),
+      body: SwitchListTile(
+        title: const Text("Dark Mode"),
+        value: themeProvider.isDarkMode,
+        onChanged: (val) {
+          context.read<ThemeProvider>().toggleTheme();
+        },
       ),
     );
   }
